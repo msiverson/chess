51f734f3ee1ae5d159484c95a69bfe2e151745d9
msiverson <msiverson321@gmail.com>
2026-03-26 18:51:33 -0600

    Handles the game logic (more will be implemented in phase 6). Just handles displaying the board for observers and players.

---
+++ client/src/main/java/states/GameSessionState.java
@@ -0,0 +1,68 @@
+package states;
+
+import chess.*;
+import client.ClientContext;
+import client.ClientState;
+import client.ServerFacade;
+import ui.ChessBoardUI;
+import ui.ChessUI;
+
+import java.util.Scanner;
+
+import static client.ClientState.POST_LOGIN;
+
+public class GameSessionState {
+
+    private final ServerFacade server; // TODO: Used phase 6
+    private final Scanner scanner;
+    private final ChessUI ui = new ChessUI();
+    private final ChessBoardUI boardUI = new ChessBoardUI();
+
+    private static final int BOARD_SIZE = 8;
+
+    public GameSessionState (ServerFacade server, Scanner scanner) {
+        this.server = server;
+        this.scanner = scanner;
+    }
+
+    public ClientState run(ClientContext context) {
+        ChessGame chessGame = new ChessGame(); // TODO: To be removed and change in phase 6
+        boardUI.draw(chessGame, context.getTeamColor(), null, null, context.getObserving());
+
+        boolean inGame = true;
+        while (inGame) {
+            System.out.print(ui.prompt(">>> "));
+            String command = scanner.nextLine();
+            if (!context.getObserving()) {
+                switch (command) {
+                    case "move", "highlight" -> System.out.println(ui.warning("Functionality not implemented"));
+                    case "leave" -> inGame = false;
+                    case "redraw" -> boardUI.draw(
+                            chessGame,
+                            context.getTeamColor(),
+                            null,
+                            null,
+                            context.getObserving()
+                    );
+                    default -> System.out.println(ui.warning("unknown command")
+                    );
+                }
+            } else {
+                switch (command) {
+                    case "leave" -> inGame = false;
+                    case "redraw" -> boardUI.draw(chessGame,
+                            context.getTeamColor(),
+                            null,
+                            null,
+                            context.getObserving()
+                    );
+                    default -> System.out.println(ui.warning("unknown command")
+                    );
+                }
+            }
+
+        }
+
+        return POST_LOGIN;
+    }
+}
